f02b633acc704c1e01b71092ded77e6694637bf3
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-02-01 23:02:24 +0500

    added all routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,44 +1,77 @@
-const express = require("express");
-const router = express.Router();
-const Subscriber = require("../models/subscribers");
+const express = require('express')
+const router = express.Router()
+const Subscriber = require('../models/subscribers')
 
-//Getting all
-router.get("/", async (req, res)=>{
-    try{
-        const subscribers = await Subscriber.find()
-        res.json(subscribers);
-    } catch(err){
-        res.status(500).json({message: err.message})
+// Getting all
+router.get('/', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find()
+    res.json(subscribers)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+// Getting One
+router.get('/:id', getSubscriber, (req, res) => {
+  res.json(res.subscriber)
+})
+
+// Creating one
+router.post('/', async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel
+  })
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+// Updating One
+router.patch('/:id', getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+// Deleting One
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.deleteOne();
+    res.json({ message: 'Deleted Subscriber' })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+async function getSubscriber(req, res, next) {
+  let subscriber
+  try {
+    console.log("Hello Beta!")
+    subscriber = await Subscriber.findById(req.params.id)
+    if (subscriber == null) {
+      return res.status(404).json({ message: 'Cannot find subscriber' })
     }
-})
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
 
-//Getting One
-router.get("/:id", (req, res)=>{
-    res.send(req.params.id);
-})
+  res.subscriber = subscriber
+  console.log(res.subscriber);
+  next()
+}
 
-//Creating One 
-router.post("/", async (req, res)=>{
-    const subscriber = new Subscriber({
-        name: req.body.name,
-        subscribedToChannel: req.body.subscribedToChannel
-    })
-    try{
-        const newSubscriber = await subscriber.save()
-        res.status(201).json(newSubscriber);
-    } catch(err){
-        res.status(400).json({message: err.message});
-    }
-})
-
-//Updating One
-router.patch("/:id", (req, res)=>{
-
-})
-
-//Deleting One
-router.patch("/:id", (req, res)=>{
-
-})
-
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-require("dotenv").config()
+require('dotenv').config()
 
-mongoose.connect(process.env.DATABASE_URL);
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
-db.on("error", (error)=>console.error(error.message))
-db.once("open", ()=>console.log("Connected to Database"))
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
-app.use(express.json());
+app.use(express.json())
 
-const subscribersRouter = require("./routes/subscribers");
-app.use("/subscribers", subscribersRouter);
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
 
-
-app.listen(3000, ()=>console.log("Server Started"))
+app.listen(3000, () => console.log('Server Started'))
